995a6b039aaec4c58dcdd9f718da4bac6b5eb164
afaqak <75029026+Afaqak@users.noreply.github.com>
2023-02-03 13:12:14 -0800

    added the post section

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,32 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 
 module.exports = {
-  content: [  "./pages/**/*.{js,ts,jsx,tsx}",
-  "./components/**/*.{js,ts,jsx,tsx}",],
+  content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: 'class', // or 'media' or 'class'
   theme: {
     screens: {
-      'xs': "370px",
-      'sm': "480px",
-      'md': "760px",
-      'lg': "1024px",
-      'xl': "1280px",
+      xs: '370px',
+      sm: '480px',
+      md: '760px',
+      lg: '1024px',
+      xl: '1280px',
     },
     extend: {
       colors: {
-        primary: "#7380ec",
-        danger: "#ff7782",
-        success: "#41f1b6",
-        warning: "#f1c041",
-        infodark: "#7d8da1",
-        infolight: "#dce1eb",
-        colordark: "#363949",
-        colorlight: "rgba(132,139,200,0.18)",
-        colorPrimaryVariant: "#111e88",
-        colorDarkVariant: "#677483",
-        colorBackground: "#f6f6f9"
+        primary: '#7380ec',
+        danger: '#ff7782',
+        success: '#41f1b6',
+        warning: '#f1c041',
+        infodark: '#7d8da1',
+        infolight: '#dce1eb',
+        colordark: '#363949',
+        colorlight: 'rgba(132,139,200,0.18)',
+        colorPrimaryVariant: '#111e88',
+        colorDarkVariant: '#677483',
+        colorBackground: '#f6f6f9',
+      },
     },
   },
-  plugins: [ require('@tailwindcss/typography'),],
-}
-}
+};
