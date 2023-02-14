278348df77e81a9356c03b4881ceb1ed9a32323a
afaqak <75029026+Afaqak@users.noreply.github.com>
2023-02-14 08:31:15 -0800

    setup the participants

--- pages/accounts/batch/group/participant/index.jsx
+++ pages/accounts/batch/group/participant/index.jsx
@@ -1,10 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
+import { selectCurrentUser } from '../../../../../src/store/user/user.selector';
 import { useRouter } from 'next/router';
 
 const Participant = () => {
   const router = useRouter();
+  const user = useSelector(selectCurrentUser);
   const { batchId, programId, groupId } = router.query;
   console.log(batchId, programId, groupId);
+  const [participants, setParticipants] = useState([]);
+  useEffect(() => {
+    const getParticipants = async () => {
+      try {
+        const res = await fetch(`https://vast-pink-moth-toga.cyclic.app/batches/${batchId}/programs/${programId}/groups/${groupId}`, {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${user.token}`,
+          },
+        });
+        const data = await res.json();
+        console.log(data);
+        setParticipants(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getParticipants();
+  }, [batchId, programId, groupId]);
+
   return <div>Participant</div>;
 };
 
